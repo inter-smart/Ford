d4604bd4a3f79a94f424e00ea20332e0e6e37b4e
ajayux <ajay@intersmart.in>
2025-06-09 11:23:37 +0530

    arabic chnages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+const createNextIntlPlugin = require("next-intl/plugin");
+
+const withNextIntl = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+module.exports = withNextIntl(nextConfig);
